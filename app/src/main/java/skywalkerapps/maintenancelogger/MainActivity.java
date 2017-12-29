bf766406c8cb774a58a8254e8497c570c0b0235a
lukeskywalkermoua <33531967+lukeskywalkermoua@users.noreply.github.com>
2017-12-29 15:10:19 -0600

    Used Firebase Realtime Database to save user input. This is done by creating a DatabaseReference instance, pointing it to the data app directory, and then calling its method to assign some value when a button is pushed etc. Next to do is to try to save user text input.

--- app/src/main/java/skywalkerapps/maintenancelogger/MainActivity.java
+++ app/src/main/java/skywalkerapps/maintenancelogger/MainActivity.java
@@ -2,13 +2,21 @@ package skywalkerapps.maintenancelogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Create an instance of Firebase Authority
-    FirebaseAuth myAuth;
+    //Create button
+    private Button myButton;
+
+    //Create a database instance
+    private DatabaseReference myDatabase;
 
 
     @Override
@@ -16,8 +24,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myAuth = FirebaseAuth.getInstance();
+        myButton = findViewById(R.id.my_button1);
 
-        //Check to see if user is currently signed in
+        //Point my data base instance to the direct root of the data app
+        myDatabase = FirebaseDatabase.getInstance().getReference();
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //When the button is clicked, the real time data is updated
+
+                //Create child in root object
+
+                //Assign some value to the child object
+
+                myDatabase.child("Name").setValue("Luke M");
+
+
+            }
+        });
+
+
+
+
     }
 }
