63cd030b9188d90d5ab50d563aae496116b6fbb4
lukeskywalkermoua <33531967+lukeskywalkermoua@users.noreply.github.com>
2018-01-01 01:21:24 -0600

    App now enables users to nickname their rides. Category will be organized/saved based on the name of the ride and/or by default via brand. Future updates could include custom settings for user to organize display. Next TO-DO is to add boolean expressions to check whether the user wants to overwrite or update vehicle info.

--- app/src/main/java/skywalkerapps/maintenancelogger/MainActivity.java
+++ app/src/main/java/skywalkerapps/maintenancelogger/MainActivity.java
@@ -8,12 +8,24 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+/**
+ * Main class that saves user input such as
+ * logging new vehicles/updating vehicle info.
+ * and storing it to the real time data Firebase
+ *
+ * Creator: Luke Moua
+ * Company: Skywalker Apps
+ * Date: 01/01/2018
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     //Create a database instance
     private DatabaseReference vehicleMakeDataRef;
     private DatabaseReference vehicleModelDataRef;
     private DatabaseReference vehicleYearDataRef;
+    private DatabaseReference vehicleOtherDataRef;
+    private DatabaseReference vehicleNickNameDataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText vehicleMakeEditText;
         final EditText vehicleModelEditText;
         final EditText vehicleYearEditText;
+        final EditText vehicleOtherDescEditText;
+        final EditText vehicleNickNameEditText;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -34,22 +48,39 @@ public class MainActivity extends AppCompatActivity {
         //Set the id of the button in xml to the Button variable
         vehicleSaveButton = findViewById(R.id.save_button);
 
-
-        //Set the id of the edit text in xml to the EditText variable
+        //Edit text box for nick name
+        vehicleNickNameEditText = findViewById(R.id.editText0);
+        //Edit text box for brand
         vehicleMakeEditText = findViewById(R.id.editText1);
-
+        //Edit text box for the vehicle brand
         vehicleModelEditText = findViewById(R.id.editText2);
-
+        //Edit text box for the year
         vehicleYearEditText = findViewById(R.id.editText3);
+        //Edit textbox for other vehicle specs.
+        vehicleOtherDescEditText = findViewById(R.id.editText4);
 
 
 
         //Point my data base instance to the direct root of the data app
         //Sets the reference category file to save the data in make/model/Year child data save
-        //The broadest category is the vehicle make(brand), then the model followed by year
-        vehicleMakeDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Make");
-        vehicleModelDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Make").child("Vehicle Model");
-        vehicleYearDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Make").child("Vehicle Model").child("Vehicle Year");
+
+        //Saves the vehicle nick name under Make/Model/Year/Other/Nickname
+        vehicleNickNameDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Nickname");
+        //Saves Vehicle Make(brand) under Make
+        vehicleMakeDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Nickname")
+                .child("Vehicle Make");
+
+        //Saves Vehicle Model under Make/Model
+        vehicleModelDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Nickname")
+                .child("Vehicle Make").child("Vehicle Model");
+
+        //Saves Vehicle year under Make/Model/Year
+        vehicleYearDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Nickname")
+                .child("Vehicle Make").child("Vehicle Model").child("Vehicle Year");
+
+        //Saves the custom vehicle description under Make/Model/Year/Other
+        vehicleOtherDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Nickname").child("Vehicle Make")
+                .child("Vehicle Model").child("Vehicle Year").child("Other Descriptions");
 
         //Button save listener for vehicle make brand) Toyota, Ford, Chevy, etc..
         vehicleSaveButton.setOnClickListener(new View.OnClickListener() {
@@ -70,13 +101,23 @@ public class MainActivity extends AppCompatActivity {
 
                 //Gets the vehicle model from user and stores it in a string
                 String stringVehicleModel = vehicleModelEditText.getText().toString().trim();
-
+                //Saves the string of vehicle model under the Vehicle Model Reference
                 vehicleModelDataRef.push().setValue(stringVehicleModel);
 
                 //Gets the vehicle year from user and stores it in a string
                 String stringVehicleYear = vehicleYearEditText.getText().toString().trim();
-                //Pushes new data for vehicle
+                //Saves the string of vehicle year under Vehicle Year reference
                 vehicleYearDataRef.push().setValue(stringVehicleYear);
+
+                //Gets other descriptions from user and stores it in a string
+                String stringVehicleOther = vehicleOtherDescEditText.getText().toString().trim();
+                //Saves the string of Other desc. under Other Descriptions
+                vehicleOtherDataRef.push().setValue(stringVehicleOther);
+
+                //Gets nickname from user and stores it in string variable
+                String stringVehicleNickName = vehicleNickNameEditText.getText().toString().trim();
+                //Saves the string of nickname under Vehicle Nickname
+                vehicleNickNameDataRef.push().setValue(stringVehicleNickName);
 
             }
         });
