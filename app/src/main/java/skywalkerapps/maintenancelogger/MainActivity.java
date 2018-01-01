e1201e04091d5d92f08a3cae32e80460e7a1e492
lukeskywalkermoua <33531967+lukeskywalkermoua@users.noreply.github.com>
2017-12-31 22:57:07 -0600

    Added new features to the vehicle maintenance app. The app now allows the user to enter in vehicle information such as the make, model, year etc. and saves all the data online via realtime-data Firebase, and can be updated anytime. Furthermore, I plan to add more vehicle spec. options as well as customizable maintenance notifications.

--- app/src/main/java/skywalkerapps/maintenancelogger/MainActivity.java
+++ app/src/main/java/skywalkerapps/maintenancelogger/MainActivity.java
@@ -4,46 +4,100 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Create button
-    private Button myButton;
-
     //Create a database instance
-    private DatabaseReference myDatabase;
-
+    private DatabaseReference vehicleMakeDataRef;
+    private DatabaseReference vehicleModelDataRef;
+    private DatabaseReference vehicleYearDataRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Create button variables for each category, make, model, year etc.
+        Button vehicleMakeButton;
+        Button vehicleModelButton;
+        Button vehicleYearButton;
+
+        //Create an edit text variable for each category
+        //TODO Future update could be to include list of already existing vehicles/equipment
+        //and then just allow user to pick from existing options
+        final EditText vehicleMakeEditText;
+        final EditText vehicleModelEditText;
+        final EditText vehicleYearEditText;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myButton = findViewById(R.id.my_button1);
+        //Set the id of the button in xml to the Button variable
+        vehicleMakeButton = findViewById(R.id.my_button1);
+
+        vehicleModelButton = findViewById(R.id.my_button2);
+
+        vehicleYearButton = findViewById(R.id.my_button3);
+
+        //Set the id of the edit text in xml to the EditText variable
+        vehicleMakeEditText = findViewById(R.id.editText1);
+
+        vehicleModelEditText = findViewById(R.id.editText2);
+
+        vehicleYearEditText = findViewById(R.id.editText3);
+
+
 
         //Point my data base instance to the direct root of the data app
-        myDatabase = FirebaseDatabase.getInstance().getReference();
+        //Sets the reference category file to save the data in make/model/Year child data save
+        //The broadest category is the vehicle make(brand), then the model followed by year
+        vehicleMakeDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Make");
+        vehicleModelDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Make").child("Vehicle Model");
+        vehicleYearDataRef = FirebaseDatabase.getInstance().getReference().child("Vehicle Make").child("Vehicle Model").child("Vehicle Year");
 
-        myButton.setOnClickListener(new View.OnClickListener() {
+        //Button save listener for vehicle make brand) Toyota, Ford, Chevy, etc..
+        vehicleMakeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //When the button is clicked, the real time data is updated
-
                 //Create child in root object
-
                 //Assign some value to the child object
+                //Retrieve the input text from the user and store
+                //it in a string variable
+                String stringVehicleMake = vehicleMakeEditText.getText().toString().trim();
 
-                myDatabase.child("Name").setValue("Luke M");
-
+                //Sets the name of the child to the string when the button is clicked
+                //push() method enables a new child to be created each time button
+                //is clicked (does not overwrite)
+                //@Params string of vehicle make/model/year
+                vehicleMakeDataRef.push().setValue(stringVehicleMake);
 
             }
         });
+        //Button save listener for vehicle model
+        vehicleModelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                //Gets the vehicle model from user and stores it in a string
+                String stringVehicleModel = vehicleModelEditText.getText().toString().trim();
+
+                vehicleModelDataRef.push().setValue(stringVehicleModel);
+
+            }
+        });
+        //Button save listener for vehicle year
+        vehicleYearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Gets the vehicle year from user and stores it in a string
+                String stringVehicleYear = vehicleYearEditText.getText().toString().trim();
+                //Creates a new year for
+                vehicleYearDataRef.push().setValue(stringVehicleYear);
+
+            }
+        });
 
 
 
